7c73ec10782cbf66dcb8101698653e49634d57d3
DzungTLab <ledungcom12@gmail.com>
2026-09-14 14:24:46 +0700

    feat: initial commit project structure

---
+++ src/main/java/com/ddagtech/aureliabooks/AureliaBooksApplication.java
@@ -0,0 +1,13 @@
+package com.ddagtech.aureliabooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AureliaBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AureliaBooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ddagtech/aureliabooks/AureliaBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddagtech.aureliabooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AureliaBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
